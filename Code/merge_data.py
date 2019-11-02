bc92071b8fa71391c3677b8368fb1877f1583b78
Jasminh <j.r.heierli@gmail.com>
2019-11-02 23:24:06 +0100

    Fixed GSI_data and typo in ListofGoods
    added more to merge_data.py

--- Code/merge_data.py
+++ Code/merge_data.py
@@ -59,10 +59,49 @@ class MergeData:
         with open(f) as csv_file:
             rows = csv.DictReader(csv_file)
             for row in rows:
-                print(row)
+                # print(row)
                 if row['country'] in self.countries:
-                    self.countries[row['prevalence_score']] = row['prevalence_score']
-                    self.countries[row['prevalence_score']] = row['prevalence_score']
+                    self.countries[row['country']][row['prevalence_score']] = row['prevalence_score']
+                    self.countries[row['country']][row['people_in_slavery']] = row['people_in_slavery']
+                    self.countries[row['country']][row['vulnerability_score']] = row['vulnerability_score']
+                    self.countries[row['country']][row['support_survivors_percentage']] = row[
+                        'support_survivors_percentage']
+                    self.countries[row['country']][row['criminal_justice_percentage']] = row[
+                        'criminal_justice_percentage']
+                    self.countries[row['country']][row['coordination_percentage']] = row['coordination_percentage']
+                    self.countries[row['country']][row['address_risk_percentage']] = row['address_risk_percentage']
+                    self.countries[row['country']][row['supply_chains_percentage']] = row['supply_chains_percentage']
+                    self.countries[row['country']][row['TOTAL']] = row['TOTAL']
+                if row['country'] == 'Russian Federation':
+                    self.countries['Russia'][row['prevalence_score']] = row['prevalence_score']
+                    self.countries['Russia'][row['people_in_slavery']] = row['people_in_slavery']
+                    self.countries['Russia'][row['vulnerability_score']] = row['vulnerability_score']
+                    self.countries['Russia'][row['support_survivors_percentage']] = row[
+                        'support_survivors_percentage']
+                    self.countries['Russia'][row['criminal_justice_percentage']] = row[
+                        'criminal_justice_percentage']
+                    self.countries['Russia'][row['coordination_percentage']] = row['coordination_percentage']
+                    self.countries['Russia'][row['address_risk_percentage']] = row['address_risk_percentage']
+                    self.countries['Russia'][row['supply_chains_percentage']] = row[
+                        'supply_chains_percentage']
+                if ',' in row['country']:
+                    country, _ = row['country'].split(',')
+                    if country in self.countries:
+                        self.countries[country][row['prevalence_score']] = row['prevalence_score']
+                        self.countries[country][row['people_in_slavery']] = row['people_in_slavery']
+                        self.countries[country][row['vulnerability_score']] = row['vulnerability_score']
+                        self.countries[country][row['support_survivors_percentage']] = row[
+                            'support_survivors_percentage']
+                        self.countries[country][row['criminal_justice_percentage']] = row[
+                            'criminal_justice_percentage']
+                        self.countries[country][row['coordination_percentage']] = row['coordination_percentage']
+                        self.countries[country][row['address_risk_percentage']] = row['address_risk_percentage']
+                        self.countries[country][row['supply_chains_percentage']] = row[
+                            'supply_chains_percentage']
+                        self.countries[country][row['TOTAL']] = row['TOTAL']
+
+                    else:
+                        continue
                 else:
                     continue
 
@@ -75,8 +114,12 @@ def main(argv):
     # print(k, v)
     print(merge_data.countries)
     print(merge_data.products)
-    # for k, v in merge_data.countries.items():
-    # print(k,v)
+    count_missing = 0
+    for k, v in merge_data.countries.items():
+        if len(v) < 3:
+            print(k)
+            count_missing += 1
+    print(count_missing)
 
 
 if __name__ == '__main__':
