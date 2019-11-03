795a882cae539d477f5193bbb85a64aa65854eb7
Jasminh <j.r.heierli@gmail.com>
2019-11-03 12:45:29 +0100

    merging news articles with countries

--- Code/sentiment_analysis.py
+++ Code/sentiment_analysis.py
@@ -2,22 +2,31 @@ import csv
 import json
 from absl import app
 from absl import flags
-from pycorenlp import StanfordCoreNLP
 import textblob
 import nltk
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_string('text_file', '/Users/JasminH/hacknlead2019/Data/texts_api.csv',
+flags.DEFINE_string('text_file', '/Users/JasminH/hacknlead2019/Data/TR_API_results.tsv',
                     '.tsv file to extract data from')
 
 
-def get_news(f):
+def get_news(f, countries):
     with open(f) as texts:
-        rows = csv.DictReader(texts)
+        rows = csv.DictReader(texts, delimiter='\t')
+        print(countries)
         for row in rows:
-            sentiment_analyis(row)
-            # print(row['parsed_text'])
+            row = sentiment_analyis(row)
+            crs = row['countries_long'].split(',')
+            for country in crs:
+                print(country)
+                try:
+                    if 'articles' in countries[country]:
+                        pass
+                    else:
+                        countries[country]['articles'] = {'guid': row['versionedguid']}
+                except:
+                    print('****OLD VERSION*****')
 
 
 def sentiment_analyis(row):
@@ -27,17 +36,28 @@ def sentiment_analyis(row):
     for sentence in sentences:
         blob = textblob.TextBlob(sentence)
         scores.append(blob.sentiment[0])
-    polarity_avg = polarity_score / len(sentences)
     dist = (0 - max(scores))
     if dist > min(scores):
         polarity = max(scores)
     else:
         polarity = min(scores)
-    print(polarity)
+    # print(polarity)
+
+    row['polarity'] = polarity
+    print(row)
+
+    return row
+
+
+def get_countries(f):
+    with open(f) as countries:
+        data = json.load(countries)
+        return data
 
 
 def main(argv):
-    get_news(FLAGS.text_file)
+    countries = get_countries('/Users/JasminH/hacknlead2019/Data/countries.json')
+    get_news(FLAGS.text_file, countries)
 
 
 if __name__ == '__main__':
